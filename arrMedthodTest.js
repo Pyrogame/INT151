15785eb6bde51eebe4b2f6208b97b32f22067b14
Pyrogame <waramate.ta@gmail.com>
2025-08-26 17:05:13 +0700

    adding words1

--- arrMedthodTest.js
+++ arrMedthodTest.js
@@ -62,5 +62,8 @@ console.log(words1);
 words1.splice(2,0,'melon','cherry')
 console.log(words1);
 
-
 const words1 = ["Mango", "Apple", "mangosteen", "orange", "mangoes"]
+//slice
+console.log(words1.slice(2))
+console.log(words1.slice(0,3))
+console.log(words1.fill(null))
